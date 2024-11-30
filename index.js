666a502568aa324a04e2369e52e24ecfca222746
Sala D14 <CSalaD14@uniboyaca.edu.co>
2024-11-30 17:08:35 +0200

    Ejercicio 2

---
+++ index.js
@@ -0,0 +1,9 @@
+const Nombre = document.getElementById("Nombre").value;
+const Categoria = document.getElementById("Categoria").value;
+const Preciou = document.getElementById("PrecioU").value;
+const Unidades = document.getElementById("Unidades").value;
+const Descuento = document.getElementById("Descuento").value;
+
+const table = '<tr><th>'{Nombre}'<th></tr>'
+
+
